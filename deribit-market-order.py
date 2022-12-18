ac6b425474c4ad345794bf0e4863e4a84f45085c
gmurray <grahammurray2802@gmail.com>
2022-12-18 23:02:03 +0000

    Latest coce, with api call on deribit test

--- deribit-market-order.py
+++ deribit-market-order.py
@@ -1,28 +1,67 @@
+import configparser
 import requests
 
-# Replace YOUR_API_KEY and YOUR_API_SECRET with your actual API key and secret
-api_key = "YOUR_API_KEY"
-api_secret = "YOUR_API_SECRET"
+# Read the configuration file
+config = configparser.ConfigParser()
+config.read('config.ini')
 
-# Set the URL for the Deribit API
-url = "https://www.deribit.com/api/v2/private/buy"
+client_id = config['DEFAULT']['client_id']
+client_secret = config['DEFAULT']['client_secret']
 
-# Set the payload for the request
-payload = {
-    "instrument_name": "BTC-PERPETUAL",
-    "amount": 1,
-    "type": "market",
-    "label": "my_market_order",
-}
+def get_access_token(client_id, client_secret):
+    # Set the URL for the Deribit API
+    url = "https://test.deribit.com/api/v2/public/auth"
 
-# Set the headers for the request
-headers = {
-    "Content-Type": "application/json",
-    "Authorization": f"Bearer {api_key}:{api_secret}",
-}
+    # Set the query parameters
+    params = {
+        "client_id": client_id,
+        "client_secret": client_secret,
+        "grant_type": "client_credentials"
+    }
 
-# Send the request to the Deribit API
-response = requests.post(url, json=payload, headers=headers)
+    # Set the headers for the request
+    headers = {
+        "Content-Type": "application/json"
+    }
 
-# Print the response from the API
-print(response.json())
+    # Send the GET request
+    response = requests.get(url, params=params, headers=headers)
+
+    # Parse the response
+    response_json = response.json()
+    #print(response_json)
+
+    # Extract the access_token field
+    access_token = response_json['result']['access_token']
+
+    # Return the access_token
+    return access_token
+
+def place_market_order(instrument_name, amount, label, access_token):
+    private_buy = "https://test.deribit.com/api/v2/private/buy"
+
+    # Set the payload for the request
+    payload = {
+        "instrument_name": instrument_name,
+        "amount": amount,
+        "type": "market",
+        "label": label
+    }
+
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer {}".format(access_token)
+    }
+
+    try:
+        # Send the request to the Deribit API
+        response = requests.get(private_buy, params=payload, headers=headers)
+        print(response)
+        response_json = response.json()
+    except requests.exceptions.RequestException as e:
+        # Print the error message if the request fails
+        print(e)
+
+# Example usage:
+access_token = get_access_token(client_id, client_secret)
+place_market_order("ETH_USDC-PERPETUAL", 0.01, "my_market_order", access_token)
