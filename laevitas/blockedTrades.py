3fdfaa6a90d02d634e473c3a0618be88f7f1d1df
gmurray <grahammurray2802@gmail.com>
2023-02-14 05:41:30 +0000

    Latest

--- laevitas/blockedTrades.py
+++ laevitas/blockedTrades.py
@@ -94,144 +94,55 @@ def extract_put_totals(instrument_name, csv_file):
 
                 return blocked_puts_total
 
-def march_call_table():
-    call_options = []
-    call_options_t1 = []
-    with open(MARCH_STRIKES_FILE, "r") as file:
+def get_options_table(expiry, option_type, strikes_file, weekly_csv_file, monthly_csv_file, weekly_csv_file_t1, monthly_csv_file_t1):
+    options = []
+    options_t1 = []
+    with open(strikes_file, "r") as file:
         for line in file:
-            instrument_name = line.strip() + "C"  #appending C for Call to the name
+            instrument_name = line.strip() + option_type  #appending option type to the name
             query_instrument_name = {"instrument_name": instrument_name}
 
-            weekly_call_totals = extract_call_totals(instrument_name, MARCH_LVT_WEEKLY_CSV_FILE)
-            monthly_call_totals = extract_call_totals(instrument_name, MARCH_LVT_MONTHLY_CSV_FILE)
-            weekly_call_totals_t1 = extract_call_totals(instrument_name, MARCH_LVT_WEEKLY_CSV_FILE_T1)
-            monthly_call_totals_t1 = extract_call_totals(instrument_name, MARCH_LVT_MONTHLY_CSV_FILE_T1)
+            weekly_totals = extract_call_totals(instrument_name, weekly_csv_file) if option_type == "C" else extract_put_totals(instrument_name, weekly_csv_file)
+            monthly_totals = extract_call_totals(instrument_name, monthly_csv_file) if option_type == "C" else extract_put_totals(instrument_name, monthly_csv_file)
+            weekly_totals_t1 = extract_call_totals(instrument_name, weekly_csv_file_t1) if option_type == "C" else extract_put_totals(instrument_name, weekly_csv_file_t1)
+            monthly_totals_t1 = extract_call_totals(instrument_name, monthly_csv_file_t1) if option_type == "C" else extract_put_totals(instrument_name, monthly_csv_file_t1)
 
-            call_options.append((instrument_name, weekly_call_totals, monthly_call_totals))
-            call_options_t1.append((instrument_name, weekly_call_totals_t1, monthly_call_totals_t1))
+            options.append((instrument_name, weekly_totals, monthly_totals))
+            options_t1.append((instrument_name, weekly_totals_t1, monthly_totals_t1))
 
-    call_options_diff = []
-    for option, option_t1 in zip(call_options, call_options_t1):
-        weekly_diff = 0 if option_t1[1] == 0 else  round((abs(option[1] - option_t1[1]) / option_t1[1]) * 100, 2)
-        monthly_diff = 0 if option_t1[2] == 0 else round((abs(option[2] - option_t1[2]) / option_t1[2]) * 100, 2)
-        call_options_diff.append((option[0], option[1], option[2], weekly_diff, monthly_diff))
+    options_diff = []
+    for opt, opt_t1 in zip(options, options_t1):
+        weekly_diff = 0 if opt_t1[1] == 0 else  round((abs(opt[1] - opt_t1[1]) / opt_t1[1]) * 100, 2)
+        monthly_diff = 0 if opt_t1[2] == 0 else round((abs(opt[2] - opt_t1[2]) / opt_t1[2]) * 100, 2)
+        options_diff.append((opt[0], opt[1], opt[2], weekly_diff, monthly_diff))
 
-    march_sorted_call_options = sorted(call_options_diff, key=lambda x: (x[2], x[1]), reverse=True)
+    sorted_options = sorted(options_diff, key=lambda x: (x[2], x[1]), reverse=True)
 
-    for option in march_sorted_call_options:
+    for option in sorted_options:
         print(f"Instrument Name: {option[0]}")
         print(f"WEEKLY CALL TOTALS: {int(option[1])}")
         print(f"WEEKLY % DIFFERENCE: {round(float(option[3]), 2)}")
         print(f"MONTHLY CALL TOTALS: {int(option[2])}")
         print(f"MONTHLY % DIFFERENCE: {round(float(option[4]), 2)}")
-    return march_sorted_call_options
+    return sorted_options
 
-def march_put_table():
-    put_options = []
-    with open(MARCH_STRIKES_FILE, "r") as file:
-        for line in file:
-            instrument_name = line.strip() + "P"  #appending P for Call to the name
-            query_instrument_name = {"instrument_name": instrument_name}
-
-            weekly_put_totals = extract_put_totals(instrument_name, MARCH_LVT_WEEKLY_CSV_FILE)
-            monthly_put_totals = extract_put_totals(instrument_name, MARCH_LVT_MONTHLY_CSV_FILE)
-
-            put_options.append((instrument_name, weekly_put_totals, monthly_put_totals))
-
-    march_sorted_put_options = sorted(put_options, key=lambda x: (x[2], x[1]), reverse=True)
-
-    for option in march_sorted_put_options:
-        print(f"Instrument Name: {option[0]}")
-        print(f"WEEKLY PUT TOTALS: {int(option[1])}")
-        print(f"MONTHLY PUT TOTALS: {int(option[2])}")
-    return march_sorted_put_options
-
-def june_call_table():
-    call_options = []
-    with open(JUNE_STRIKES_FILE, "r") as file:
-        for line in file:
-            instrument_name = line.strip() + "C"  #appending P for Call to the name
-            query_instrument_name = {"instrument_name": instrument_name}
-
-            weekly_call_totals = extract_call_totals(instrument_name, JUNE_LVT_WEEKLY_CSV_FILE)
-            monthly_call_totals = extract_call_totals(instrument_name, JUNE_LVT_MONTHLY_CSV_FILE)
-            call_options.append((instrument_name, weekly_call_totals, monthly_call_totals))
-
-    june_sorted_call_options = sorted(call_options, key=lambda x: (x[2], x[1]), reverse=True)
-
-    for option in june_sorted_call_options:
-        print(f"Instrument Name: {option[0]}")
-        print(f"WEEKLY CALL TOTALS: {int(option[1])}")
-        print(f"MONTHLY CALL TOTALS: {int(option[2])}")
-    return june_sorted_call_options
-
-def june_put_table():
-    put_options = []
-    with open(JUNE_STRIKES_FILE, "r") as file:
-        for line in file:
-            instrument_name = line.strip() + "P"  #appending P for Call to the name
-            query_instrument_name = {"instrument_name": instrument_name}
-
-            weekly_put_totals = extract_put_totals(instrument_name, JUNE_LVT_WEEKLY_CSV_FILE)
-            monthly_put_totals = extract_put_totals(instrument_name, JUNE_LVT_MONTHLY_CSV_FILE)
-            put_options.append((instrument_name, weekly_put_totals, monthly_put_totals))
-
-    june_sorted_put_options = sorted(put_options, key=lambda x: (x[2], x[1]), reverse=True)
-
-    for option in june_sorted_put_options:
-        print(f"Instrument Name: {option[0]}")
-        print(f"WEEKLY PUT TOTALS: {int(option[1])}")
-        print(f"MONTHLY PUT TOTALS: {int(option[2])}")
-    return june_sorted_put_options
-
-def september_call_table():
-    call_options = []
-    with open(SEPTEMBER_STRIKES_FILE, "r") as file:
-        for line in file:
-            instrument_name = line.strip() + "C"  #appending P for Call to the name
-            query_instrument_name = {"instrument_name": instrument_name}
-
-            weekly_call_totals = extract_call_totals(instrument_name, SEPTEMBER_LVT_WEEKLY_CSV_FILE)
-            monthly_call_totals = extract_call_totals(instrument_name, SEPTEMBER_LVT_MONTHLY_CSV_FILE)
-            call_options.append((instrument_name, weekly_call_totals, monthly_call_totals))
-
-    september_sorted_call_options = sorted(call_options, key=lambda x: (x[2], x[1]), reverse=True)
-
-    for option in september_sorted_call_options:
-        print(f"Instrument Name: {option[0]}")
-        print(f"WEEKLY CALL TOTALS: {int(option[1])}")
-        print(f"MONTHLY CALL TOTALS: {int(option[2])}")
-    return september_sorted_call_options
-
-def september_put_table():
-    put_options = []
-    with open(SEPTEMBER_STRIKES_FILE, "r") as file:
-        for line in file:
-            instrument_name = line.strip()
-            query_instrument_name = {"instrument_name": instrument_name}
-
-            weekly_put_totals = extract_put_totals(instrument_name, SEPTEMBER_LVT_WEEKLY_CSV_FILE)
-            monthly_put_totals = extract_put_totals(instrument_name, SEPTEMBER_LVT_MONTHLY_CSV_FILE)
-            put_options.append((instrument_name, weekly_put_totals, monthly_put_totals))
-
-    september_sorted_put_options = sorted(put_options, key=lambda x: (x[2], x[1]), reverse=True)
-
-    for option in september_sorted_put_options:
-        print(f"Instrument Name: {option[0]}")
-        print(f"WEEKLY PUT TOTALS: {int(option[1])}")
-        print(f"MONTHLY PUT TOTALS: {int(option[2])}")
-    return september_sorted_put_options
+march_call_table = get_options_table("MARCH", "C", MARCH_STRIKES_FILE, MARCH_LVT_WEEKLY_CSV_FILE, MARCH_LVT_MONTHLY_CSV_FILE, MARCH_LVT_WEEKLY_CSV_FILE_T1, MARCH_LVT_MONTHLY_CSV_FILE_T1)
+march_put_table = get_options_table("MARCH", "P", MARCH_STRIKES_FILE, MARCH_LVT_WEEKLY_CSV_FILE, MARCH_LVT_MONTHLY_CSV_FILE, MARCH_LVT_WEEKLY_CSV_FILE_T1, MARCH_LVT_MONTHLY_CSV_FILE_T1)
+june_call_table = get_options_table("JUNE", "C", JUNE_STRIKES_FILE, JUNE_LVT_WEEKLY_CSV_FILE, JUNE_LVT_MONTHLY_CSV_FILE, JUNE_LVT_WEEKLY_CSV_FILE_T1, JUNE_LVT_MONTHLY_CSV_FILE_T1)
+june_put_table = get_options_table("JUNE", "P", JUNE_STRIKES_FILE, JUNE_LVT_WEEKLY_CSV_FILE, JUNE_LVT_MONTHLY_CSV_FILE, JUNE_LVT_WEEKLY_CSV_FILE_T1, JUNE_LVT_MONTHLY_CSV_FILE_T1)
+september_call_table = get_options_table("SEPTEMBER", "C", SEPTEMBER_STRIKES_FILE, SEPTEMBER_LVT_WEEKLY_CSV_FILE, SEPTEMBER_LVT_MONTHLY_CSV_FILE, SEPTEMBER_LVT_WEEKLY_CSV_FILE_T1, SEPTEMBER_LVT_MONTHLY_CSV_FILE_T1)
+september_put_table = get_options_table("SEPTEMBER", "P", SEPTEMBER_STRIKES_FILE, SEPTEMBER_LVT_WEEKLY_CSV_FILE, SEPTEMBER_LVT_MONTHLY_CSV_FILE, SEPTEMBER_LVT_WEEKLY_CSV_FILE_T1, SEPTEMBER_LVT_MONTHLY_CSV_FILE_T1)
 
 @app.route('/blocked-bets')
 def push_web():
-    march_calls = march_call_table()
-    march_puts = march_put_table()
+    march_calls = march_call_table
+    march_puts = march_put_table
     march_pc_ratio = put_call_ratio(march_calls, march_puts)
-    june_calls = june_call_table()
-    june_puts = june_put_table()
+    june_calls = june_call_table
+    june_puts = june_put_table
     june_pc_ratio = put_call_ratio(june_calls, june_puts)
-    sep_calls = september_call_table()
-    sep_puts = september_put_table()
+    sep_calls = september_call_table
+    sep_puts = september_put_table
     sep_pc_ratio = put_call_ratio(sep_calls, sep_puts)
     return render_template('table.html', data_call_march=march_calls, data_put_march=march_puts, data_march_pc_ratio=march_pc_ratio, data_call_june=june_calls, data_put_june=june_puts, data_june_pc_ratio=june_pc_ratio, data_call_sep=sep_calls, data_put_sep=sep_puts, data_sep_pc_ratio=sep_pc_ratio)
 
