b99e18c3bd22dce6384bd78fffc2b63db33ffc29
gmurray <grahammurray2802@gmail.com>
2023-03-13 10:55:17 +0000

    Latest

--- options-high-iv-search/ivp.py
+++ options-high-iv-search/ivp.py
@@ -1,6 +1,32 @@
+import configparser
 import requests
 import time
 import pandas as pd
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# Constants
+HISTORICAL_LOW_IV = 15.55
+HISTORICAL_HIGH_IV = 185.90
+
+# Data Directory
+DIR = "/home/gmurray/REPO/trading/data-weekly"
+
+# Read the configuration file
+config = configparser.ConfigParser()
+config.read('/home/gmurray/REPO/trading/config.ini')
+
+client_id = config['DEFAULT']['client_id']
+client_secret = config['DEFAULT']['client_secret']
+
+def calculate_ivr(mark_iv):
+    return (mark_iv - HISTORICAL_LOW_IV) / (HISTORICAL_HIGH_IV - HISTORICAL_LOW_IV) * 100
+
+def calculate_ivp(mark_iv):
+    df = pd.read_csv(f"{DIR}/historical_eth_iv.csv", encoding="utf-8", sep=',')
+    ivp = df.query("HV < @mark_iv").count()[0]/365*100
+    return ivp
 
 # Set the API endpoint for BTC and ETH index prices
 btc_get_index_endpoint = "https://www.deribit.com/api/v2/public/get_index?currency=BTC"
@@ -55,31 +81,53 @@ expiries = [d for d in data
             and d["expiration_timestamp"] - now <= 80*86400*1000]
 
 # Filter the expiries based on strikes less than 0.30 delta
-filtered_exp = []
+filtered_exp_lt_50 = []
+filtered_exp_lt_80 = []
+
 for exp in expiries:
     endpoint = f"https://www.deribit.com/api/v2/public/get_order_book_by_instrument_id?instrument_id={exp['instrument_id']}"
     response = requests.get(endpoint)
     order_book = response.json()
     if "result" in order_book and order_book['result'] and order_book['result']['open_interest']:
         delta = order_book['result']['greeks']['delta']
-        if 0.15 < abs(delta) < 0.30:
-            #print(exp["base_currency"])
+        if 0.15 < abs(delta) < 0.32:
             index_price = btc_index_price if exp["base_currency"] == "BTC" else eth_index_price
-            #print(index_price)
-            range_min, range_max = calculate_range(index_price, order_book['result']['mark_iv'])
-            filtered_exp.append({
+            mark_iv = order_book['result']['mark_iv']
+            range_min, range_max = calculate_range(index_price, mark_iv)
+            filtered_exp_dict = {
                 "instrument_name": exp["instrument_name"],
-                "mark_iv": order_book['result']['mark_iv'],
+                "mark_iv": mark_iv,
                 "option_type": exp["option_type"],
                 "strike": exp["strike"],
                 "delta": delta,
                 "volume": order_book["result"]['stats']["volume"],
                 "open_interest": order_book["result"]["open_interest"],
-                "range": f"{range_min:.0f} - {range_max:.0f}"
-            })
+                "range": f"{range_min:.0f} - {range_max:.0f}",
+                "days_to_expiry": (pd.to_datetime(exp['expiration_timestamp'], unit='ms') - pd.Timestamp.now()).days,
+                "ivr": calculate_ivr(mark_iv),
+                "ivp": calculate_ivp(mark_iv),
+            }
+            if filtered_exp_dict["days_to_expiry"] < 50:
+                filtered_exp_lt_50.append(filtered_exp_dict)
+            elif filtered_exp_dict["days_to_expiry"] < 80:
+                filtered_exp_lt_80.append(filtered_exp_dict)
 
-# Convert the filtered data to a pandas DataFrame
-df = pd.DataFrame(filtered_exp, columns=["instrument_name", "mark_iv", "option_type", "strike", "delta", "volume", "open_interest", "range"])
+df_lt_50 = pd.DataFrame(filtered_exp_lt_50, columns=["instrument_name", "mark_iv", "ivr", "ivp", "option_type", "strike", "delta", "volume", "open_interest", "range", "days_to_expiry"])
+df_lt_80 = pd.DataFrame(filtered_exp_lt_80, columns=["instrument_name", "mark_iv", "ivr", "ivp", "option_type", "strike", "delta", "volume", "open_interest", "range", "days_to_expiry"])
+
+df_lt_50_sorted = df_lt_50.sort_values(by=['ivp', 'ivr'], ascending=[False, False])
+df_lt_80_sorted = df_lt_80.sort_values(by=['ivp', 'ivr'], ascending=[False, False])
+
+data_lt_50 = df_lt_50_sorted.to_dict('records')
+data_lt_80 = df_lt_80_sorted.to_dict('records')
+
 
 # Print the DataFrame
-print(df)
+#print(df_sorted)
+
+@app.route('/ivp')
+def ivp():
+    return render_template('table.html', data_less_than_50=data_lt_50, data_less_than_80=data_lt_80)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5008)
